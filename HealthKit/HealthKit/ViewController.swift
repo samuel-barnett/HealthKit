276e2e590062d6c051c16b7efa9fc2b948aa9015
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2025-02-24 17:29:38 -0500

    added xcode project

---
+++ HealthKit/HealthKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthKit
+//
+//  Created by Barnett, Samuel on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HealthKit/HealthKitTests/HealthKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitTests.swift
+//  HealthKitTests
+//
+//  Created by Barnett, Samuel on 2/24/25.
+//
+
+import Testing
+@testable import HealthKit
+
+struct HealthKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
